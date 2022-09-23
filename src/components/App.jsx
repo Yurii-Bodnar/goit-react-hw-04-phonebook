8f0f85be0711569a95656eeacdf37ef05aed0a94
Yurii-Bodnar <yuriy.bodnar75@gmail.com>
2022-09-23 18:06:25 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,71 +1,60 @@
-import { Component } from 'react';
 import PhonebookForm from './PhonebookForm/PhonebookForm';
 import Contacts from './Contacts/Contacts';
 import Filter from './Filter/Filter';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
-export class App extends Component {
-  state = {
-    contacts: [
+export const App = () => {
+  const [contacts, setContacts] = useState(
+    JSON.parse(window.localStorage.getItem('contacts')) ?? [
       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
+    ]
+  );
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  useEffect(() => {
+    const contacts = localStorage.getItem('contacts');
+    const parseContacts = JSON.parse(contacts);
+    if (parseContacts) {
+      setContacts(parseContacts);
+    }
+  }, []);
+
+  const eventFilterContacts = e => {
+    setFilter(e.target.value);
   };
 
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts')
-    const parseContacts = JSON.parse(contacts)
-    if(parseContacts){
-      this.setState({contacts:parseContacts})
-    }
-  }
-  componentDidUpdate(prevProps, prevState) {
-    if(this.state.contacts !== prevState.contacts){
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
-    }
-  }
-
-  eventFilterContacts = e => {
-    this.setState({ filter: e.target.value });
+  const addContacts = contacts => {
+    setContacts(prewState => [...prewState, contacts]);
   };
 
-  addContacts = contacts => {
-    this.setState(prewState => {
-      return {
-        contacts: [...prewState.contacts, contacts],
-      };
+  const getVisibleContacts = () => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact => {
+      return contact.name.toLowerCase().includes(normalizedFilter);
     });
   };
 
-  getVisibleContacts = () => {
-    const normalizedFilter = this.state.filter.toLowerCase();
-    return this.state.contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizedFilter)
-    );
+  const handleDeleteContact = id => {
+    setContacts(contacts.filter(contact => contact.id !== id));
   };
-  handleDeleteContact = (id) => {
-    this.setState(prewState => ({
-      contacts: prewState.contacts.filter(contact => contact.id !== id),
-    }))
-  }
 
-  render() {
-    const { filter } = this.state;
-    const visibleContact = this.getVisibleContacts();
+  const visibleContact = getVisibleContacts();
 
-    return (
-      <>
-        <h1>Phonebook</h1>
-        <PhonebookForm
-          contacts={this.state.contacts}
-          addContacts={this.addContacts}
-        />
-        <h2>Contacts</h2>
-        <Filter value={filter} filterContacts={this.eventFilterContacts} />
-        <Contacts contacts={visibleContact} deleteContact={this.handleDeleteContact}/>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <h1>Phonebook</h1>
+      <PhonebookForm contacts={contacts} addContacts={addContacts} />
+      <h2>Contacts</h2>
+      <Filter value={filter} filterContacts={eventFilterContacts} />
+      <Contacts contacts={visibleContact} deleteContact={handleDeleteContact} />
+    </>
+  );
+};
--- src/components/PhonebookForm/PhonebookForm.jsx
+++ src/components/PhonebookForm/PhonebookForm.jsx
@@ -1,25 +1,34 @@
 import PropTypes from 'prop-types';
 import { nanoid } from 'nanoid';
-import { Component } from 'react';
-import css from './PhonebookForm.module.css'
+import { useState } from 'react';
+import css from './PhonebookForm.module.css';
+import Notiflix from 'notiflix';
 
-class PhonebookForm extends Component {
-  state = {
-    name: '',
-    number: '',
-  };
-  handleChange = e => {
+const PhonebookForm = ({ contacts, addContacts }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleChange = e => {
     const { name, value } = e.target;
-    this.setState({ [name]: value });
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+      default:
+        return;
+    }
   };
 
-  reset = () => {
-    this.setState({ name: '', number: '' });
+  const reset = () => {
+    // this.setState({ name: '', number: '' });
+    setName('');
+    setNumber('');
   };
 
-  handleSubmitt = e => {
-    const { contacts, addContacts } = this.props;
-    const { name, number } = this.state;
+  const handleSubmitt = e => {
     e.preventDefault();
     const contact = { id: nanoid(), name: name, number: number };
     if (
@@ -27,54 +36,56 @@ class PhonebookForm extends Component {
         return contact.name === name;
       }).length
     ) {
-      return alert(`${name} in already in contacts`);
+      return Notiflix.Notify.warning(`${name} in already in contacts`);
     }
     addContacts(contact);
-    this.reset();
+    reset();
   };
 
-  render() {
-    const id = nanoid();
+  const id = nanoid();
 
-    return (
-      <>
-        <form className={css.container} onSubmit={this.handleSubmitt}>
-          <label htmlFor={id}>
-            <p className={css.text}>Name</p>
-            <input className={css.inputValue}
-              onChange={this.handleChange}
-              type="text"
-              name="name"
-              value={this.state.name}
-              id={this.id}
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              required
-            />
-          </label>
-          <label htmlFor={id}>
-            <p className={css.text}>Number</p>
-            <input className={css.inputValue}
-              onChange={this.handleChange}
-              type="tel"
-              name="number"
-              value={this.state.number}
-              id={id}
-              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-              required
-            />
-          </label>
-          <button className={css.btn} type="submit">add conttact</button>
-        </form>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <form className={css.container} onSubmit={handleSubmitt}>
+        <label htmlFor={id}>
+          <p className={css.text}>Name</p>
+          <input
+            className={css.inputValue}
+            onChange={handleChange}
+            type="text"
+            name="name"
+            value={name}
+            id={id}
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </label>
+        <label htmlFor={id}>
+          <p className={css.text}>Number</p>
+          <input
+            className={css.inputValue}
+            onChange={handleChange}
+            type="tel"
+            name="number"
+            value={number}
+            id={id}
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+        </label>
+        <button className={css.btn} type="submit">
+          add conttact
+        </button>
+      </form>
+    </>
+  );
+};
 
 PhonebookForm.propTypes = {
-    contacts:PropTypes.arrayOf(PropTypes.shape(PropTypes.string.isRequired,)), 
-    addContacts:PropTypes.func.isRequired,
-}
+  contacts: PropTypes.arrayOf(PropTypes.shape(PropTypes.string.isRequired)),
+  addContacts: PropTypes.func.isRequired,
+};
 
 export default PhonebookForm;
